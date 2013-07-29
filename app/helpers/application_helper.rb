408a4ad36254fe892a6cb8fb9edec9019349bedd
Meredith Underell <m.underell@gmail.com>
2013-07-29 10:14:06 -0700

    Add title functionality for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "TravelAppCf").join(" - ")
+      end
+    end
+  end
+
 end
--- spec/integration/creating_destinations_spec.rb
+++ spec/integration/creating_destinations_spec.rb
@@ -8,5 +8,10 @@ feature 'Creating Destinations' do
     fill_in 'Country', :with => 'Canada'
     click_button 'Create Destination'
     page.should have_content('Destination has been created.')
+
+    destination = Destination.find_by_city("Vancouver")
+    page.current_url.should == destination_url(destination)
+    title = "Vancouver - Destinations - TravelAppCf"
+    find("title").should have_content(title)
   end
 end
